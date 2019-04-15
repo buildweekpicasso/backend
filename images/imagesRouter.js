6c6ee2b3aaf70c54d6ea092b2365ccdecf61afe5
andrewlowell <andrewlowellr@gmail.com>
2019-04-15 13:56:34 -0600

    Finish error codes and messages for login and registration.

--- images/imagesRouter.js
+++ images/imagesRouter.js
@@ -1,8 +1,20 @@
 const router = require('express').Router();
-const images = require('./imagesRouter.js');
+const images = require('./imagesModel.js');
+const ImageUtils = require('./imageUtils.js');
 
-router.post('/', (req, res) => {
-  
+router.post('/process', (req, res) => {
+  console.log('posting the process thing?');
+  const { style, content } = req.body;
+  console.log(style, content);
+  ImageUtils.processDeepAI(style, content)
+    .then(image => {
+      console.log(image);
+      res.status(200).json({ url: image });
+    })
+    .catch(err => {
+      console.log('uh oh', err);
+      res.status(500).json({ error: err });
+    })
 });
 
-module.export = router;
+module.exports = router;
