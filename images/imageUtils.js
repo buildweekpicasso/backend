c494d69d5d965379f072995d210ecfe56e3a58f6
Peter Murphy <peter.francis.murphy@gmail.com>
2019-04-16 19:36:26 -0400

    Revert request_key back to uuid

--- images/imageUtils.js
+++ images/imageUtils.js
@@ -4,7 +4,7 @@ const process = ({ fast, request_key, style_url, content_url }) => {
   console.log({ request_key, style_url, content_url });
   return fetch(`http://3.14.3.46:5000/${fast ? 'fast' : 'deep'}transform`, {
     method: 'post',
-    body: JSON.stringify({ request_key: '111', style_url, content_url }),
+    body: JSON.stringify({ request_key, style_url, content_url }),
     headers: { 'Content-Type': 'application/json' },
   }).then(res => res.json());
 };
