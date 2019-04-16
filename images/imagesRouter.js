34919f226de9fecb1be7e6f04dc916f9e752149a
Peter Murphy <peter.francis.murphy@gmail.com>
2019-04-16 16:18:19 -0400

    Update imagesRouter to reflect change in endpoint

--- images/imagesRouter.js
+++ images/imagesRouter.js
@@ -5,6 +5,8 @@ const uuid = require('uuid/v4');
 const images = require('./imagesModel.js');
 const ImageUtils = require('./imageUtils.js');
 
+const BASE_URL = process.env.HEROKU_URL || 'https://bw-picasso.herokuapp.com/';
+
 const fileFilter = (_req, file, cb) => {
   file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)
     ? cb(null, true)
@@ -41,21 +43,6 @@ router.post('/upload', (req, res) => {
   });
 });
 
-// @TODO: This logic will need to be combined with
-// what I've got as the "upload" route above, but
-// for the sake of mocking this out, I think we can
-// leave separate for now
-// router.post('/process', (req, res) => {
-//   const { style, content } = req.body;
-//   ImageUtils.processDeepAI(style, content)
-//     .then(image => {
-//       res.status(200).json(image);
-//     })
-//     .catch(err => {
-//       res.status(500).json({ error: err });
-//     });
-// });
-
 router.post('/process', (req, res) => {
   upload(req, res, err => {
     if (err) {
@@ -69,10 +56,14 @@ router.post('/process', (req, res) => {
     images
       .findStyleById(parseInt(styleID, 10))
       .then(style => {
-        console.log('static/styles/' + style.imageUrl, req.file.path);
-        const styleURL = 'static/styles/' + style.imageUrl;
-
-        ImageUtils.processDeepAI(styleURL, req.file.path)
+        const style_url = `${BASE_URL}styles/${style.imageUrl}`;
+        const content_url = `${BASE_URL}styles/${req.file.filename}`;
+        ImageUtils.process({
+          fast: true,
+          request_key: '23123',
+          style_url,
+          content_url,
+        })
           .then(image => {
             res.status(200).json(image);
           })
