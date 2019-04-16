dece5f63d8ddcdfdc828a45e4f6bddb4213282db
Peter Murphy <peter.francis.murphy@gmail.com>
2019-04-16 01:21:23 -0400

    Iterate on styles table

--- data/migrations/20190415220311_add_styles_table.js
+++ data/migrations/20190415220311_add_styles_table.js
@@ -2,7 +2,10 @@ exports.up = knex =>
   knex.schema.createTable('styles', styles => {
     styles.increments();
 
-    styles.string('url').notNullable();
+    styles.string('imageUrl').notNullable();
+    styles.string('thumbUrl').notNullable();
+    styles.string('year').notNullable();
+    styles.string('title').notNullable();
   });
 
 exports.down = knex => knex.schema.dropTableIfExists('styles');
--- data/seeds/01-styles.js
+++ data/seeds/01-styles.js
@@ -1,6 +1,17 @@
 const fs = require('fs');
+const { chunksOf } = require('../../lib');
 
 exports.seed = knex => {
-  const files = fs.readdirSync('./static/styles').map(url => ({ url }));
-  knex('styles').insert(files);
+  const files = fs.readdirSync('./static/styles');
+  const styles = chunksOf(2)(files).map(([imageUrl, thumbUrl]) => ({
+    imageUrl,
+    thumbUrl,
+    year: imageUrl
+      .split(',')
+      .map(f_ => f_.replace(/^_/, '').replace(/_/g, ' '))[1],
+    title: imageUrl
+      .split(',')
+      .map(f_ => f_.replace(/^_/, '').replace(/_/g, ' '))[2],
+  }));
+  return knex('styles').insert(styles);
 };
