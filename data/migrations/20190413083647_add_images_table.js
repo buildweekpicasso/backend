6532dae40aaec7112a4bbb4e4c8f9fb9e9160066
Peter Murphy <peter.francis.murphy@gmail.com>
2019-04-16 00:01:24 -0400

    Add styles migration

--- data/migrations/20190413083647_add_images_table.js
+++ data/migrations/20190413083647_add_images_table.js
@@ -2,17 +2,12 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('images', images => {
     images.increments();
 
-    images.integer('user_id')
+    images
+      .integer('user_id')
       .references('id')
       .inTable('users')
       .onDelete('CASCADE')
       .notNullable();
-
-    images
-      .binary('data')
-      .notNullable();
-
-    images.boolean('proccessed');
   });
 };
 
--- data/migrations/20190415220311_add_styles_table.js
+++ data/migrations/20190415220311_add_styles_table.js
@@ -2,11 +2,7 @@ exports.up = knex =>
   knex.schema.createTable('styles', styles => {
     styles.increments();
 
-    styles
-      .string('url')
-      .onUpdate('CASCADE')
-      .onDelete('CASCADE')
-      .notNullable();
+    styles.string('url').notNullable();
   });
 
 exports.down = knex => knex.schema.dropTableIfExists('styles');
