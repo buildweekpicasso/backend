62819a4c527aa8310f0068daa7d8ea273863071b
Peter Murphy <peter.francis.murphy@gmail.com>
2019-04-17 10:29:29 -0400

    Add migration for user_images

---
+++ data/migrations/20190417102337_user_images.js
@@ -0,0 +1,33 @@
+exports.up = knex =>
+  knex.schema.createTable('user_images', user_images => {
+    user_images.increments();
+
+    user_images
+      .integer('user_id')
+      .references('id')
+      .inTable('users')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE')
+      .notNullable();
+    user_images
+      .integer('image_id')
+      .references('id')
+      .inTable('images')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE')
+      .notNullable();
+    user_images
+      .integer('style_id')
+      .references('id')
+      .inTable('styles')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE')
+      .notNullable();
+
+    user_images
+      .string('request_key')
+      .unique()
+      .notNullable();
+  });
+
+exports.down = knex => knex.schema.dropTableIfExists('user_images');
