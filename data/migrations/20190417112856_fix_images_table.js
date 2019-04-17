ac7691a204db0f369ed637488c83bbf33a9c0a69
Peter Murphy <peter.francis.murphy@gmail.com>
2019-04-17 11:31:13 -0400

    Add migration for image_url on images table

---
+++ data/migrations/20190417112856_fix_images_table.js
@@ -0,0 +1,13 @@
+exports.up = knex =>
+  knex.schema.hasTable('images').then(
+    exists =>
+      exists &&
+      knex.schema.table('images', images => {
+        images
+          .string('image_url', 256)
+          .notNullable()
+          .unique();
+      }),
+  );
+
+exports.down = () => {};
