fd022e632d79aae08b63d75d8b531d01d076c1df
Peter Murphy <peter.francis.murphy@gmail.com>
2019-04-17 11:40:59 -0400

    Add user_images seed

---
+++ data/seeds/04-user_images.js
@@ -0,0 +1,12 @@
+exports.seed = knex => {
+  const userImages = [
+    {
+      output_url: 'example.com/image.png',
+      user_id: 1,
+      image_id: 1,
+      style_id: 1,
+      request_key: '123-abc',
+    },
+  ];
+  return knex('user_images').insert(userImages);
+};
