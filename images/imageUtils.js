1fb140fa54219341dedc63105380bc6afde69f73
Peter Murphy <peter.francis.murphy@gmail.com>
2019-04-17 13:22:42 -0400

    Update DS endpoint URL

--- images/imageUtils.js
+++ images/imageUtils.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 
 const process = ({ fast, request_key, style_url, content_url }) => {
   console.log({ request_key, style_url, content_url });
-  return fetch(`http://3.14.3.46:5000/${fast ? 'fast' : 'deep'}transform`, {
+  return fetch(`http://18.217.10.89:5000/${fast ? 'fast' : 'deep'}transform`, {
     method: 'post',
     body: JSON.stringify({ request_key, style_url, content_url }),
     headers: { 'Content-Type': 'application/json' },
