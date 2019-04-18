98a8fb4fafa844d6b0a4c135f72c4bcf6ed3c894
Peter Murphy <peter.francis.murphy@gmail.com>
2019-04-18 12:17:32 -0400

    Remove image FK from public_images migration

--- data/migrations/20190418115841_add_public_images_table.js
+++ data/migrations/20190418115841_add_public_images_table.js
@@ -3,13 +3,6 @@ exports.up = knex =>
     public_images.increments();
 
     public_images
-      .integer('image_id')
-      .references('id')
-      .inTable('images')
-      .onUpdate('CASCADE')
-      .onDelete('CASCADE')
-      .notNullable();
-    public_images
       .integer('style_id')
       .references('id')
       .inTable('styles')
@@ -22,7 +15,8 @@ exports.up = knex =>
       .unique()
       .notNullable();
 
-    public_images.string('output_url').notNullable();
+    public_images.string('content_url', 256).notNullable();
+    public_images.string('output_url', 256).notNullable();
   });
 
 exports.down = knex => knex.schema.dropTableIfExists('public_images');
